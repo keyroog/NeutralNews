8593c25970f1c1f0a0087715e52002894bbe017e
Salvatore Sirica <keyroog@hotmail.com>
2023-02-18 17:44:59 +0100

    fix keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  storageAccountKey: '+mk/tp6gumSm5cNVUovtkIG1Pu6M6HOxvXu/9MzcTzmfG8iBWu7fxUGeZDz2rP84DLtzB5d9s4UV+ASt10VWOQ==',
-  bingSubscriptionKey: 'fb8327747c314a639861f32926553f83',
-  cognitiveSubscriptionKey: 'b139c1002d6f4df58d6a39939fdca6d7',
-  loginUrl : 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectlogin?code=Qph3Jloal2HGmbHlUVqhPzSNKcA0novLsuJfBBqg6DwPAzFufEGnVQ==',
-  signupUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectsignup?code=bncpyLBBxgFE3CQR5dscBEahRETWMD_P1h-DLA5mJi39AzFuYvNgnQ==',
-  updateUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectupdate?',
-  searchUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectsearch?',
-  populateUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectpopulatehome?',
-};
+export const environment = {};
 
 /*
  * For easier debugging in development mode, you can import the following file
