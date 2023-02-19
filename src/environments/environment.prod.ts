d06bb5c991d910737d9e020cef68bb18d926fb39
Salvatore Sirica <keyroog@hotmail.com>
2023-02-19 15:11:46 +0100

    favicon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,1 @@
-export const environment = {
-  production: true,
-  storageAccountKey: '+mk/tp6gumSm5cNVUovtkIG1Pu6M6HOxvXu/9MzcTzmfG8iBWu7fxUGeZDz2rP84DLtzB5d9s4UV+ASt10VWOQ==',
-  bingSubscriptionKey: 'fb8327747c314a639861f32926553f83',
-  cognitiveSubscriptionKey: 'b139c1002d6f4df58d6a39939fdca6d7',
-  loginUrl : 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectlogin?code=Qph3Jloal2HGmbHlUVqhPzSNKcA0novLsuJfBBqg6DwPAzFufEGnVQ==',
-  signupUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectsignup?code=bncpyLBBxgFE3CQR5dscBEahRETWMD_P1h-DLA5mJi39AzFuYvNgnQ==',
-  updateUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectupdate?',
-  searchUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectsearch?',
-  populateUrl: 'https://cpfunctionresource.azurewebsites.net/api/cloudprojectpopulatehome?',
-};
+export const environment = {};
